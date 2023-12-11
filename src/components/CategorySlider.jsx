505376ac55e298f43e80dfd47d3f3a9f9f409c54
pomerama <marpom@gmail.com>
2023-12-11 09:57:52 +0100

    created CategorySlider component which is diplsayed on the homepage

---
+++ src/components/CategorySlider.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import categoryService from "../services/category.service";
+
+function CategorySlider(props) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        categoryService
+            .getCategoriesList()
+            .then((response) => {
+                setCategories(response.data.slice(0, 10));
+            })
+            .catch((error) => {
+                console.log("API: Error while getting the list of games")
+                const errorDescription = error.response.data.message;
+                setErrorMessage(errorDescription);
+            })
+    }, []);
+
+    return (
+        <div>
+            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slider="true">
+                <ul className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-grid">
+
+                    {categories && categories.map((category) => {
+                        return (
+
+                            <li key={category._id} >
+                                <Link to={`/categories/details/${category._id}`}>
+                                    <div className="uk-card uk-card-default uk-card-body category-card">
+                                        <h3 className="uk-card-title">
+
+                                            {category.name}
+                                        </h3>
+                                        <p>{category.description}</p>
+                                    </div>
+                                </Link>
+                            </li>
+
+
+                        )
+                    })}
+                </ul>
+
+                <a className="uk-position-center-left uk-position-small uk-hidden-hover" href="true" uk-slidenav-previous="true" uk-slider-item="previous"></a>
+                <a className="uk-position-center-right uk-position-small uk-hidden-hover" href="true" uk-slidenav-next="true" uk-slider-item="next"></a>
+
+            </div>
+
+        </div >
+    )
+}
+
+export default CategorySlider;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import gameService from "../services/game.service";
+import CategorySlider from "../components/CategorySlider";
 
 function HomePage() {
     const [games, setGames] = useState([]);
-    const [errorMessage, setErrorMessage] = useState(undefined);
 
     useEffect(() => {
         gameService
@@ -13,50 +13,16 @@ function HomePage() {
                 setGames(response.data);
             })
             .catch((error) => {
-                console.log("API: Error while getting the list of games")
                 const errorDescription = error.response.data.message;
-                setErrorMessage(errorDescription);
+                console.log("API: Error while getting the list of games ", errorDescription)
             })
     }, []);
 
     return (
         <div className="uk-container uk-text-center homepage">
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
             <h1>Games Wiki</h1>
 
-            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slider="true">
-                <ul className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-grid">
-                    <li>
-                        <div className="uk-card uk-card-default uk-card-body category-card">
-                            <h3 className="uk-card-title">Default</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="uk-card uk-card-default uk-card-body category-card">
-                            <h3 className="uk-card-title">Default</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="uk-card uk-card-default uk-card-body category-card">
-                            <h3 className="uk-card-title">Default</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="uk-card uk-card-default uk-card-body category-card">
-                            <h3 className="uk-card-title">Default</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div>
-                    </li>
-                </ul>
-
-                <a className="uk-position-center-left uk-position-small uk-hidden-hover" href="true" uk-slidenav-previous="true" uk-slider-item="previous"></a>
-                <a className="uk-position-center-right uk-position-small uk-hidden-hover" href="true" uk-slidenav-next="true" uk-slider-item="next"></a>
-
-            </div>
-
+            <CategorySlider></CategorySlider>
 
 
             <Link to="/games/">
